200b6f8299d8ca71c7f103f8c4dbcf5a3b3ad0d6
Douglas M. Negri <douglasmnegri@Douglass-Laptop.local>
2024-10-24 21:26:33 -0300

    add a projects section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./styles.css";
-const greeting = "Hello! This is my homepage!"
+const greeting = "Hello! This is my homepage!";
 console.log(greeting);
+
+function showProject(project) {
+  const projectTag = document.querySelector(project);
+  projectTag.addEventListener("click", () => {
+    console.log("Hello World!");
+  });
+}
+
+showProject("book");
